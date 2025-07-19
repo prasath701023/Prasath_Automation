3f68f0c2b9edadba596e43f18a247157fff86e27
prasath701023 <hariprasathr920@gmail.com>
2025-07-19 21:32:56 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		//version of type float
+		float values=91.0f;
+		System.out.println(values);
+		System.out.println("************************");
+		
+		// developer of type String
+		String name="Google";
+		System.out.println(name);
+		System.out.println("************************");
+		
+		//Beta of type boolean
+		boolean Beta=false;
+		System.out.println(Beta);
+		System.out.println("************************");
+		
+		//releaseYear of type int
+		int year=2008;
+		System.out.println(year);
+		System.out.println("************************");
+		
+		//shortcutKey of type char
+		char shortcutkey='c';
+		System.out.println(shortcutkey);
+				
+		
+
+	}
+
+}
